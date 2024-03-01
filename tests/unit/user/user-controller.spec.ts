8898e6e1c329e908d40cac551016477f9d98a4e9
renanrider <renan.silva112@fatec.sp.gov.br>
2024-03-01 06:26:28 -0300

    test: ensure UserController return 500 if  add user fails

---
+++ tests/unit/user/user-controller.spec.ts
@@ -0,0 +1,56 @@
+import { faker } from '@faker-js/faker';
+import {
+  AddUser,
+  AddUserParams,
+  AddUserResult,
+} from '@/domain/usecases/user/add-user';
+import {
+  UserController,
+  UserRequest,
+} from '@/presentation/controllers/user-controller';
+import { serverError } from '@/presentation/helpers/http-status-code';
+
+const mockRequest = (): UserRequest => ({
+  name: faker.person.firstName(),
+  email: faker.internet.email(),
+  address: faker.location.streetAddress(),
+});
+
+export class AddUserSpy implements AddUser {
+  addUserParams: AddUserParams;
+
+  constructor(addUserParams: AddUserParams) {
+    this.addUserParams = addUserParams;
+  }
+
+  add(addUserParams: AddUserParams): Promise<AddUserResult | null> {
+    this.addUserParams = addUserParams;
+
+    return Promise.resolve(this.addUserParams);
+  }
+}
+
+interface SutTypes {
+  sut: UserController;
+  addUserSpy: AddUserSpy;
+}
+
+const makeSut = (): SutTypes => {
+  const addUserSpy = new AddUserSpy(mockRequest());
+  const sut = new UserController(addUserSpy);
+  return {
+    sut,
+    addUserSpy,
+  };
+};
+
+describe('UserController', () => {
+  test('Should return 500 if add user fails', async () => {
+    const { sut, addUserSpy } = makeSut();
+    jest.spyOn(addUserSpy, 'add').mockRejectedValueOnce(new Error());
+    const request = mockRequest();
+    const httpResponse = await sut.handle(request);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
+});
