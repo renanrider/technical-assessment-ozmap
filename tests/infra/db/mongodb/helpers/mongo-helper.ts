1be5f8ce904e721edbbf36ffc0dacaf2dc432789
renanrider <renan.silva112@fatec.sp.gov.br>
2024-03-07 19:12:56 -0300

    refactor: add getCollection method

--- tests/infra/db/mongodb/helpers/mongo-helper.ts
+++ tests/infra/db/mongodb/helpers/mongo-helper.ts
@@ -11,4 +11,8 @@ export const MongoHelper = {
   async disconnect(): Promise<void> {
     await this.client.close();
   },
+
+  async getCollection(name: string): Promise<any> {
+    return this.client.db().collection(name);
+  },
 };
