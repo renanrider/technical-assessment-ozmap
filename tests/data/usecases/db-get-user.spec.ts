65d21f59d38a8f676c5b84f03b9bf5cac0341a77
renanrider <renan.silva112@fatec.sp.gov.br>
2024-03-07 11:39:28 -0300

    test: ensure return null if call UserRepository error

---
+++ tests/data/usecases/db-get-user.spec.ts
@@ -0,0 +1,51 @@
+import { User } from '@/domain/models/user';
+import { GetUserParams, GetUserRepository } from '@/data/protocols/db/user';
+import { DbGetUser } from '@/data/usecases';
+
+const mockRequest = (): GetUserParams => ({
+  userId: '1',
+});
+
+const mockResponse = (): User => ({
+  id: '1',
+  name: 'any_name',
+  email: 'any_email@email.com',
+  address: 'any_address',
+  coordinates: [0, 0],
+});
+
+class DbGetUserSpy implements GetUserRepository {
+  findById(getUserParams: GetUserParams): Promise<User | null> {
+    if (getUserParams.userId === '1') {
+      return Promise.resolve(mockResponse());
+    } else {
+      return Promise.resolve(null);
+    }
+  }
+}
+
+type SutTypes = {
+  sut: DbGetUser;
+  dbGetUserSpy: DbGetUserSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const dbGetUserSpy = new DbGetUserSpy();
+  const sut = new DbGetUser(dbGetUserSpy);
+  return {
+    dbGetUserSpy,
+    sut,
+  };
+};
+
+describe('DbGetUser usecase', () => {
+  it('return null if call UserRepository error', async () => {
+    const { sut, dbGetUserSpy } = makeSut();
+    jest.spyOn(dbGetUserSpy, 'findById').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const promise = await sut.get(mockRequest());
+    expect(promise).toBeNull();
+  });
+});
