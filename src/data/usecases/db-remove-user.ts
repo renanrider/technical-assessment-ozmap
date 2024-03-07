7897454d179ff52e8a6e00aeb1c77caca245c644
renanrider <renan.silva112@fatec.sp.gov.br>
2024-03-07 15:01:27 -0300

    refactor: simplify the code

--- src/data/usecases/db-remove-user.ts
+++ src/data/usecases/db-remove-user.ts
@@ -3,31 +3,14 @@ import {
   RemoveUserParams,
   RemoveUserResult,
 } from '@/domain/usecases/user/remove-user';
-import { GetUserRepository } from '@/data/protocols/db/user';
 import { RemoveUserRepository } from '../protocols/db/user';
 
 export class DbRemoveUser implements RemoveUser {
-  constructor(
-    private getUserRepository: GetUserRepository,
-    private removeUserRepository: RemoveUserRepository,
-  ) {}
+  constructor(private removeUserRepository: RemoveUserRepository) {}
 
   async remove(
-    getUserParams: RemoveUserParams,
+    removeUserParams: RemoveUserParams,
   ): Promise<RemoveUserResult | null> {
-    try {
-      const user = await this.getUserRepository.findById(getUserParams);
-
-      if (!user) throw new Error('user not found');
-
-      try {
-        await this.removeUserRepository.remove(getUserParams);
-        return user;
-      } catch (error) {
-        return null;
-      }
-    } catch (error) {
-      return null;
-    }
+    return await this.removeUserRepository.remove(removeUserParams);
   }
 }
