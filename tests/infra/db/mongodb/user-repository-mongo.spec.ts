8cfd9fb62af2edee12417f3c6d058a92eadc9687
renanrider <renan.silva112@fatec.sp.gov.br>
2024-03-11 12:03:23 -0300

    test: return all users when GetAllUsersRepository.findAll

--- tests/infra/db/mongodb/user-repository-mongo.spec.ts
+++ tests/infra/db/mongodb/user-repository-mongo.spec.ts
@@ -1,4 +1,7 @@
-import { AddUserRepositoryMongo } from '@/infra/db/mongodb';
+import {
+  AddUserRepositoryMongo,
+  GetAllUsersRepositoryMongo,
+} from '@/infra/db/mongodb';
 import { MongoHelper } from '@/infra/db/mongodb/mongo-helper';
 import { User } from '../../../domain/models';
 
@@ -7,6 +10,24 @@ const userMock: User = {
   email: 'any_email@email.com',
   address: 'any_address',
 };
+
+const usersMock: User[] = [
+  {
+    name: 'user1_name',
+    email: 'user1_email@email.com',
+    address: 'user1_address',
+  },
+  {
+    name: 'user2_name',
+    email: 'user2_email@email.com',
+    address: 'user2_address',
+  },
+  {
+    name: 'user3_name',
+    email: 'user3_email@email.com',
+    address: 'user3_address',
+  },
+];
 
 describe('AddUserRepositoryMongo', () => {
   beforeAll(async () => {
@@ -30,5 +51,23 @@ describe('AddUserRepositoryMongo', () => {
     expect(user.id).toBeTruthy();
     expect(user.name).toBe(userMock.name);
     expect(user.email).toBe(userMock.email);
+  });
+
+  it('return all users when GetAllUsersRepository.findAll', async () => {
+    const addUserRepositoryMongo = new AddUserRepositoryMongo();
+
+    await addUserRepositoryMongo.create(usersMock[0]);
+    await addUserRepositoryMongo.create(usersMock[1]);
+    await addUserRepositoryMongo.create(usersMock[2]);
+
+    const sut = new GetAllUsersRepositoryMongo();
+    const users = await sut.findAll();
+
+    expect(users).toBeTruthy();
+    expect(users).toHaveLength(3);
+    expect(users![0].id).toBeTruthy();
+    expect(users![0].name).toBe(usersMock[0].name);
+    expect(users![0].email).toBe(usersMock[0].email);
+    expect(users![0].address).toBe(usersMock[0].address);
   });
 });
