db13cbc1374e3a129a3aaf137f38e7776ba1fccf
renanrider <renan.silva112@fatec.sp.gov.br>
2024-03-11 16:01:57 -0300

    feat: setup server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,32 @@
 import dotenv from 'dotenv';
-dotenv.config();
+import { MongoHelper } from '../infra/db/mongodb/mongo-helper';
 
-import { app } from '../main/config/app';
+const setupEnviromentVariables = () => {
+  dotenv.config();
+  const { MONGO_URI, DB_USER, DB_PASSWORD, PORT } = process.env;
 
-const { PORT } = process.env;
+  if (!MONGO_URI || !DB_USER || !DB_PASSWORD || !PORT) {
+    throw new Error('Missing environment variables');
+  }
 
-app.listen(PORT, () =>
-  console.log(`Server running at http://localhost:${PORT}`),
-);
+  return { MONGO_URI, DB_USER, DB_PASSWORD, PORT };
+};
+
+const startServer = async () => {
+  try {
+    const { MONGO_URI, DB_USER, DB_PASSWORD, PORT } =
+      setupEnviromentVariables();
+    await MongoHelper.connect(MONGO_URI, DB_USER, DB_PASSWORD);
+
+    const { setupApp } = await import('./config/app');
+    const app = await setupApp();
+
+    app.listen(PORT, () =>
+      console.log(`Server running at http://localhost:${PORT}`),
+    );
+  } catch (error) {
+    console.error('Failed to start server:', error);
+  }
+};
+
+startServer();
